d2af06fafb456d8a3cdc2fb8b1c2cab72a8fa95f
Adrian Lopez Gonzalez <algo@equinor.com>
2025-03-25 18:22:25 +0100

    fix: simplify script as factory config options has been simplified

--- src/linters/eslint/scripts/typegen.ts
+++ src/linters/eslint/scripts/typegen.ts
@@ -1,65 +1,40 @@
-import { builtinRules } from 'eslint/use-at-your-own-risk';
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { flatConfigsToRulesDTS } from 'eslint-typegen/core';
 import fs from 'node:fs/promises';
-import { combine } from '../utils';
-import type { TypedFlatConfigItem } from '../types';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { builtinRules } from 'eslint/use-at-your-own-risk';
+import { flatConfigsToRulesDTS } from 'eslint-typegen/core';
 import { base } from '../configs/base';
 import { imports } from '../configs/imports';
 import { jsdoc } from '../configs/jsdoc';
+import { typescript } from '../configs/typescript';
 import { unicorn } from '../configs/unicorn';
-
-/**
- * Extracts rule lists from an array of linter configurations and generates a TypeScript interface.
- *
- * This function takes an array of linter configuration objects, extracts rule IDs from each,
- * and formats them into a TypeScript interface where each configuration name maps to a union type
- * of its respective rule names.
- *
- * @param configs - An array of linter configuration objects.
- * @returns A string representation of a TypeScript interface defining the rule lists.
- */
-function flatConfigsToRuleListDTS(configs: TypedFlatConfigItem[]) {
-  const rules = new Map<string, Set<string>>();
-  for (const config of configs) {
-    if (!config.name) continue;
-    const ruleList = new Set<string>();
-    for (const [ruleId, rule] of Object.entries(config.rules ?? {})) {
-      ruleList.add(ruleId);
-    }
-    if (ruleList.size > 0) rules.set(config.name ?? '', ruleList);
-  }
-  const ruleList = Array.from(rules).map(([key, ruleSet]) => {
-    const values = Array.from(ruleSet).map(
-      (rule) => `"${rule}"?: RuleOptions["${rule}"]`
-    );
-    return `\t"${key}": {${values.join(',')}}`;
-  });
-  return `export interface RuleList {\n${ruleList.join(',\n')}\n}`;
-}
+import { combine } from '../utils';
 
 // Define configs
 const configs = await combine(
   {
     plugins: {
       '': {
+        // eslint-disable-next-line @typescript-eslint/no-deprecated -- Deprecated but still needed, no known alternative.
         rules: Object.fromEntries(builtinRules.entries()),
       },
     },
   },
   base(),
+  typescript({ tsconfigPath: './tsconfig.json' }), // It doesn't matter if the file doesn't exist is just for the typegen generation.
   imports(),
   jsdoc(),
   unicorn()
 );
 
+const configNames = configs.map((i) => i.name).filter(Boolean);
+
 // Generate typegen.d.ts
 const dts = await flatConfigsToRulesDTS(configs, {
   includeAugmentation: false,
 });
-const dtsWithRileList = dts.concat('\n\n', flatConfigsToRuleListDTS(configs));
+const dtsWithRileList = `${dts}\n\n// Names of all the configs\nexport type ConfigNames = ${configNames.map((i) => `'${i}'`).join(' | ')};`;
 
 // Write typegen.d.ts
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 await fs.writeFile(`${__dirname}/../typegen.d.ts`, dtsWithRileList);

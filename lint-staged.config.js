38edbb9af03feb99adcf6ee8dc486062e70de31a
Adrian Lopez Gonzalez <algo@equinor.com>
2025-04-24 11:07:33 +0200

    chore: simplify lint-staged and add prettier to format scripts as we have now a hybrid setup

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,5 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  '*.{html,md,yaml,yml}': ['prettier --write'],
   '*': [() => 'bun run format:all', () => 'bun run lint'],
 };
